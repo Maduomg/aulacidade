a950621567686a4a9b7109c1d2a460bddb2238dd
Maduomg <mariagferreira2312@gmail.com>
2025-03-20 14:58:13 -0300

    Criando controler Cadastro

---
+++ aulacidade/Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aulacidade.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
