29b181db10cafce24a8b94bf2b699b5221670e7b
Maduomg <mariagferreira2312@gmail.com>
2025-03-20 15:32:41 -0300

    Adicionando o mysql

--- aulacidade/Controllers/LoginController.cs
+++ aulacidade/Controllers/LoginController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using aulacidade.Models;
+using aulacidade.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aulacidade.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public LoginController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha == senha)
+            {
+                // Autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
+
+        }
+
     }
 }
